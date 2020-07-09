6213a9c8c466be6e3bb492bb679964578dc263e9
shanmukh5w0 <17211a05w0@bvrit.ac.in>
2020-07-09 17:28:39 +0530

    last commit

--- app.js
+++ app.js
@@ -5,24 +5,28 @@ app.listen(process.env.PORT||3000, function(){
     console.log("server connected");
 });
 app.use(express.static("frontend"));
+
 app.use("/addition",function(req,res){
     let num1 = (req.query.a);
     let num2 = (req.query.b);
     let c = parseInt(num1)+parseInt(num2);
     res.send({result:c});
 })
+
 app.use("/subtraction",function(req,res){
     let num1 = (req.query.a);
     let num2 = (req.query.b);
     let c = parseInt(num1)-parseInt(num2);
     res.send({result:c});
 })
+
 app.use("/multiplication",function(req,res){
     let num1 = (req.query.a);
     let num2 = (req.query.b);
     let c = parseInt(num1)*parseInt(num2);
     res.send({result:c});
 })
+
 app.use("/division",function(req,res){
     let num1 = (req.query.a);
     let num2 = (req.query.b);
